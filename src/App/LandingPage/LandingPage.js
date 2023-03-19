edc019432ae199de5c9aedad3e8f14eddef1d82f
damskw <damian.skwierawski@gmail.com>
2023-03-19 22:23:10 +0100

    add a bit of base structure

---
+++ src/App/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import NavBar from "./NavBar/NavBar";
+
+const LandingPage = props => {
+
+    return (
+        <div>
+            <NavBar/>
+            <h1>This is landing page.</h1>
+        </div>
+    )
+}
+
+
+export default LandingPage;
---
+++ src/App/LandingPage/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar = props => {
+
+    return (
+        <div>
+            <h1>This is navbar.</h1>
+        </div>
+    )
+}
+
+
+export default NavBar;
